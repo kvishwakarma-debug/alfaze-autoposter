434f2577c1befe7867651e59a8e8b77da619ff09
Kamal Vishwakarma <72241917+kvishwakarma-debug@users.noreply.github.com>
2026-08-03 13:15:44 +0530

    Update facebook_publisher.py

--- publishers/facebook_publisher.py
+++ publishers/facebook_publisher.py
@@ -1,12 +1,21 @@
-import requests, time
+import requests
 
 def post_to_fb_reel(video_url, page_id, access_token, caption):
     print(f"Posting FB Video: {video_url}")
-    # FB Page pe reel nahi, normal video ke roop me post karo - 100% kaam karta hai
     r = requests.post(f"https://graph.facebook.com/v20.0/{page_id}/videos", data={
         "file_url": video_url,
         "description": caption,
         "access_token": access_token
     }).json()
     print("FB Video Publish:", r)
+    return r
+
+def post_to_fb_feed(image_url, page_id, access_token, caption):
+    print(f"Posting FB Feed Photo: {image_url}")
+    r = requests.post(f"https://graph.facebook.com/v20.0/{page_id}/photos", data={
+        "url": image_url,
+        "caption": caption,
+        "access_token": access_token
+    }).json()
+    print("FB Feed Publish:", r)
     return r
